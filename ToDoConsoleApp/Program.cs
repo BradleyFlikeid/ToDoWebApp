500f0424d7de9e72ceec6903749b0811ad649571
bradley.flikeid <bradley.flikeid@FLIKEID8-X1>
2016-05-06 08:47:24 -0500

    Add project files.

---
+++ ToDoConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ToDoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute().Wait();
+        }
+
+        public async static Task Execute()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("http://todowebapp20160505124210.azurewebsites.net/api/values/4");
+            string content = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
